d5b5c87a2564d90f02e808533182212db25bdd99
381706-1KolesovaChristina <43133706+381706-1KolesovaChristina@users.noreply.github.com>
2019-10-22 19:27:00 +0300

    Update close_value_vector.h

--- modules/task_1/kolesova_k_close_value_vector/close_value_vector.h
+++ modules/task_1/kolesova_k_close_value_vector/close_value_vector.h
@@ -1,6 +1,6 @@
 // Copyright 2019 Kolesova Kristina
-#ifndef MODULES_TASK_1_KOLESOVA_K_CLOSE_VALUE_VECTOR_CLOSE_VALUE_VECTOR_H
-#define MODULES_TASK_1_KOLESOVA_K_CLOSE_VALUE_VECTOR_CLOSE_VALUE_VECTOR_H
+#ifndef MODULES_TASK_1_KOLESOVA_K_CLOSE_VALUE_VECTOR_CLOSE_VALUE_VECTOR_H_
+#define MODULES_TASK_1_KOLESOVA_K_CLOSE_VALUE_VECTOR_CLOSE_VALUE_VECTOR_H_
 
 #include <vector>
 
@@ -8,4 +8,4 @@ std::vector<int> getRandomVector(int size);
 int getMinDifferenceOneProc(const std::vector <int> Vec);
 int getMinDifference(const std::vector <int> Vec);
 
-#endif  // MODULES_TASK_1_KOLESOVA_K_CLOSE_VALUE_VECTOR_CLOSE_VALUE_VECTOR_H
+#endif  // MODULES_TASK_1_KOLESOVA_K_CLOSE_VALUE_VECTOR_CLOSE_VALUE_VECTOR_H_
